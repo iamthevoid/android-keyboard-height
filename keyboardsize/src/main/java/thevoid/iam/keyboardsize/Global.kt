dd046d6c5431928e2da61ff0bf90056a90e7d1d2
iamthevoid <tarabrin@healbe.com>
2018-05-25 10:56:52 +0300

    updated context fetching

--- keyboardsize/src/main/java/thevoid/iam/keyboardsize/Global.kt
+++ keyboardsize/src/main/java/thevoid/iam/keyboardsize/Global.kt
@@ -7,7 +7,8 @@ import android.content.ContextWrapper
 fun getActivity(context: Context): Activity {
     return when (context) {
         is Activity -> context
-        is ContextWrapper -> getActivity(context)
+        is ContextWrapper -> getActivity(context.baseContext)
         else -> throw IllegalStateException("Context $context NOT contains activity!")
     }
 }
+//((ContextWrapper) context).getBaseContext()
